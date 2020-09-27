3a79aafba4072549a8157279d38305c04914a64b
be39na <61196684+be39na@users.noreply.github.com>
2020-09-27 23:17:13 +0300

    Sheuling method's

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -1,10 +1,11 @@
 #include "Matrix.h"
 #include <omp.h>
 #include <chrono>
-void Multiply(Matrix*, Matrix*, Matrix*);
+
 void MultiplyLinear(Matrix*, Matrix*, Matrix*);
-
-
+void MultiplyStatic(Matrix*, Matrix*, Matrix* );
+void MultiplyDynamic(Matrix*, Matrix*, Matrix* );
+void MultiplyGuided(Matrix*, Matrix*, Matrix* );
 int main()
 {
 	Matrix *firstMatrix, *secondMatrix;
@@ -15,49 +16,40 @@ int main()
 	firstMatrix = new Matrix();
 	resultMatrix = new Matrix();
 	secondMatrix = new Matrix();
+	int startSize = 500;
+	for (int i = 0; i < 6; i++) {
 
 
-
-	firstMatrix->GetRandomMatrixToFile(3, 3, "../11.txt",10);
-	secondMatrix->GetRandomMatrixToFile(3, 3, "../22.txt",10);
-
-
-
-
+		std::cout << startSize << " Size" << std::endl;
+		firstMatrix->GetRandomMatrixToFile(startSize, startSize, "../11.txt", 10);
+		secondMatrix->GetRandomMatrixToFile(startSize, startSize, "../22.txt", 10);
 
 
 #pragma omp parallel sections
-	{
-#pragma omp section
 		{
-			firstMatrix->GetMatrixFromFile("../11.txt");  // Ввод исходных матриц
-		}
 #pragma omp section
-		{
-			secondMatrix->GetMatrixFromFile("../22.txt");
+			{
+				firstMatrix->GetMatrixFromFile("../11.txt");  // Ввод исходных матриц
+			}
+#pragma omp section
+			{
+				secondMatrix->GetMatrixFromFile("../22.txt");
+			}
 		}
+
+
+
+
+		MultiplyDynamic(firstMatrix, secondMatrix, resultMatrix);
+		MultiplyStatic(firstMatrix, secondMatrix, resultMatrix);
+		MultiplyGuided(firstMatrix, secondMatrix, resultMatrix);
+		MultiplyLinear(firstMatrix, secondMatrix, resultMatrix);
+		//resultMatrix->DebugOutpurMatrix();
+
+		resultMatrix->WriteToFile("../result.txt");
+
+		startSize += 100;
 	}
-
-
-	MultiplyLinear(firstMatrix, secondMatrix,resultMatrix);
-	resultMatrix->DebugOutpurMatrix();
-
-	resultMatrix->WriteToFile("../result.txt");
-	//firstMatrix.GetMatrixFromFile("S:\\11.txt");
-	//secondMatrix.GetMatrixFromFile("S:\\22.txt");
-	/*if (resultMatrix = Multiply(firstMatrix, secondMatrix))
-	{
-		//resultMatrix->DebugOutpurMatrix(); 
-		
-		resultMatrix->WriteToFile("../result.txt");  // Вывод результата
-	}*/
-	/*else
-	{
-		std::cout << "Failed...";
-	}*/
-
-
-
 
 	
 
@@ -66,7 +58,7 @@ std:getchar();
 	return 0;
 }
 
-void Multiply(Matrix* firstMatrix, Matrix* secondMatrix,Matrix* resultMatrix)
+void MultiplyStatic(Matrix* firstMatrix, Matrix* secondMatrix,Matrix* resultMatrix)
 {
 	try
 	{
@@ -101,7 +93,7 @@ void Multiply(Matrix* firstMatrix, Matrix* secondMatrix,Matrix* resultMatrix)
 			}
 		}
 		std::chrono::duration<double>  time = std::chrono::system_clock::now() - start;
-		std::cout << "time:" << time.count() << std::endl;
+		std::cout << "Static time:" << time.count() << std::endl;
 
 	}
 	catch (const char* str)
@@ -112,6 +104,96 @@ void Multiply(Matrix* firstMatrix, Matrix* secondMatrix,Matrix* resultMatrix)
 
 }
 
+void MultiplyGuided(Matrix* firstMatrix, Matrix* secondMatrix, Matrix* resultMatrix)
+{
+	try
+	{
+		std::cout << "Paralel:\n";
+		auto  start = std::chrono::system_clock::now();
+		if (firstMatrix->N != secondMatrix->M)  throw "can't multiply n1 not equal m2";
+		resultMatrix->Init(firstMatrix->M, secondMatrix->N);
+
+
+		int str;
+		int stolb;
+		int k;
+		int i;
+
+
+
+#pragma omp parallel
+		{
+
+#pragma omp for private(str, stolb, k, i) schedule(guided)
+
+			for (i = 0; i < resultMatrix->M * resultMatrix->N; i++)
+			{
+				str = i % resultMatrix->M;
+				stolb = i / resultMatrix->M;
+
+
+				for (k = 0; k < firstMatrix->N; k++)
+				{
+					resultMatrix->matrix[str][stolb] += firstMatrix->matrix[str][k] * secondMatrix->matrix[k][stolb];
+				}
+			}
+		}
+		std::chrono::duration<double>  time = std::chrono::system_clock::now() - start;
+		std::cout << "Guided time:" << time.count() << std::endl;
+
+	}
+	catch (const char* str)
+	{
+		std::cout << str << std::endl;
+		return;
+	}
+}
+
+void MultiplyDynamic(Matrix* firstMatrix, Matrix* secondMatrix, Matrix* resultMatrix)
+{
+	try
+	{
+		std::cout << "Paralel:\n";
+		auto  start = std::chrono::system_clock::now();
+		if (firstMatrix->N != secondMatrix->M)  throw "can't multiply n1 not equal m2";
+		resultMatrix->Init(firstMatrix->M, secondMatrix->N);
+
+
+		int str;
+		int stolb;
+		int k;
+		int i;
+
+
+
+#pragma omp parallel
+		{
+
+#pragma omp for private(str, stolb, k, i) schedule(dynamic)
+
+			for (i = 0; i < resultMatrix->M * resultMatrix->N; i++)
+			{
+				str = i % resultMatrix->M;
+				stolb = i / resultMatrix->M;
+
+
+				for (k = 0; k < firstMatrix->N; k++)
+				{
+					resultMatrix->matrix[str][stolb] += firstMatrix->matrix[str][k] * secondMatrix->matrix[k][stolb];
+				}
+			}
+		}
+		std::chrono::duration<double>  time = std::chrono::system_clock::now() - start;
+		std::cout << "Dynamic time:" << time.count() << std::endl;
+
+	}
+	catch (const char* str)
+	{
+		std::cout << str << std::endl;
+		return;
+	}
+
+}
 
 void MultiplyLinear(Matrix* firstMatrix, Matrix* secondMatrix,Matrix *resultMatrix)
 {
@@ -138,9 +220,9 @@ void MultiplyLinear(Matrix* firstMatrix, Matrix* secondMatrix,Matrix *resultMatr
 
 				for (k = 0; k < firstMatrix->N; k++)
 				{
-					std::cout << firstMatrix->matrix[str][k]<<"+"<<secondMatrix->matrix[k][stolb];
+					
 					resultMatrix->matrix[str][stolb] += firstMatrix->matrix[str][k] * secondMatrix->matrix[k][stolb];
-					std::cout <<"===" <<resultMatrix->matrix[str][stolb] << std::endl;
+					
 					//resultMatrix->matrix[str][stolb] = k;
 				}
 			}
